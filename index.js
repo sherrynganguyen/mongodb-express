85cf1fd855af9ffd7f7ecf112958a698c7c320f7
Nga Nguyen <ngasdragon@gmail.com>
2020-05-30 19:29:41 -0700

    Initialized connection to MongoDB data

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+const DBUSER = process.env.DBUSER;
+const DBPASSWORD = process.env.DBPASSWORD;
+
+const url = `mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0-onasa.mongodb.net/mongodb-express?retryWrites=true&w=majority`;
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+  (!err) ? console.log("Connected successfully to database") : console.log('Error connecting to database');
+})
 app.use('/', () => {
   console.log('Learning MongDB')
 });
